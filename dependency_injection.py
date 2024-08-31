7e907b62d7d986d58a777ebc7194a2c77bc16c51
akosarev <kosarev_andrey@mail.ru>
2024-08-31 12:44:16 +0200

    dependency_injection fixed overwriting params

--- dependency_injection.py
+++ dependency_injection.py
@@ -3,16 +3,18 @@ from typing import Callable
 
 
 def value_generator():
-    return random.randint(1, 5)
+    return random.randint(1, 100)
 
 
 def dependency_injector(**fs: Callable):
     def inner(func):
         def wrapper(*args, **kwargs):
-            di_args = {k:v() for k, v in fs.items() }
-            result = func(*args, **kwargs, **di_args)
+            di_args = {k: v() for k, v in fs.items()}
+            result = func(*args, **{**di_args, **kwargs})
             return result
+
         return wrapper
+
     return inner
 
 
@@ -26,8 +28,19 @@ def func_2(val, dep):
     print(f"func2 received {dep=}, {val=}")
 
 
-for _ in range(10):
+@dependency_injector(value=value_generator, value2=value_generator)
+def func_3(value, value2):
+    print(f"func2 received {value=}, {value2=}")
+
+
+# USAGE
+# simple injection
+for _ in range(5):
     func()
 
-
+# combine injection with arguments
 func_2(5)
+
+# overwrite the dependency injection when needed
+func_3(value2=15)
+func_3(value=0, value2=15)
